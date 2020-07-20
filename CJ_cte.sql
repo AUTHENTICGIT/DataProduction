3e319267ee49b0429fbdace6fcdfbc03460b2738
AUTHENTIC <leogwork@gmail.com>
2020-07-20 17:31:17 +0800

    CengJi · SQL CTE

--- CJ_cte.sql
+++ CJ_cte.sql
@@ -1,10 +1,11 @@
+--LR: https://www.cnblogs.com/yanggb/p/11165276.html
 SELECT
 	count( * ) AS _count
 FROM
-	(
+	( --LR: 定义了两个CTE
 		WITH RECURSIVE XLY_TEMP_TABLE AS (
 		  SELECT DISTINCT * FROM ( SELECT * FROM `M_ypzxw_m_userinfo_1_18` ) AS TABLE_VIEW
-		),
+		),  --LR: 逗号隔开两个CTE：CTE后面跟其他CTE，但只能使用一个WITH，多个CTE用逗号隔开
 		COL_CTE ( `id`, `pid`, `xlyShowName`, `xlyLevel` ) AS (
       SELECT
         `id`,
@@ -12,7 +13,7 @@ FROM
         `id`,
         1 AS `xlyLevel`
       FROM
-        XLY_TEMP_TABLE
+        XLY_TEMP_TABLE  --LR: CTE可以引用自身，也可以引用同一个WITH子句中预先定义的CTE，但不允许向前引用（定义钱引用）
       WHERE
         CONCAT( `pid` ) = '0' AND CONCAT( `id` ) != CONCAT( `pid` )
 
