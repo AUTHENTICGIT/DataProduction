bdf8c5b6111a71294f9faad4e1262abc2a35d212
AUTHENTIC <leogwork@gmail.com>
2020-07-20 17:16:58 +0800

    CengJi · SQL CTE

---
+++ CJ_cte.sql
@@ -0,0 +1,34 @@
+SELECT
+	count( * ) AS _count
+FROM
+	(
+		WITH RECURSIVE XLY_TEMP_TABLE AS ( SELECT DISTINCT * FROM ( SELECT * FROM `M_ypzxw_m_userinfo_1_18` ) AS TABLE_VIEW ),
+		COL_CTE ( `id`, `pid`, `xlyShowName`, `xlyLevel` ) AS (
+		SELECT
+			`id`,
+			`pid`,
+			`id`,
+			1 AS `xlyLevel`
+		FROM
+			XLY_TEMP_TABLE
+		WHERE
+			CONCAT( `pid` ) = '0'
+			AND CONCAT( `id` ) != CONCAT( `pid` ) UNION ALL
+		SELECT
+			TEMP_CTE_TABLE.`id`,
+			TEMP_CTE_TABLE.`pid`,
+			TEMP_CTE_TABLE.`id`,
+			TEMP_CTE_TABLE2.`xlyLevel` + 1 AS `xlyLevel`
+		FROM
+			XLY_TEMP_TABLE AS TEMP_CTE_TABLE
+			INNER JOIN COL_CTE AS TEMP_CTE_TABLE2 ON CONCAT( TEMP_CTE_TABLE.`pid` ) = CONCAT( TEMP_CTE_TABLE2.`id` )
+			AND CONCAT( TEMP_CTE_TABLE.`pid` ) != CONCAT( TEMP_CTE_TABLE2.`pid` ) -- 防止子节点再指向父节点的情况
+
+			AND CONCAT( TEMP_CTE_TABLE2.`id` ) IS NOT NULL
+			AND LENGTH( trim( TEMP_CTE_TABLE2.`id` ) ) > 0 -- 防止子节点为空的情况
+
+		) SELECT DISTINCT
+		*
+	FROM
+	COL_CTE
+	) AS A
